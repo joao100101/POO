7412ee204431b0ab4aca08c301f76df847d9bde1
João Victor Mundel <joaovitor20002@gmail.com>
2022-10-25 10:58:35 -0300

    Add files via upload

--- ApiBD/src/main/java/com/joaovictormundel/api/controller/CandidatoController.java
+++ ApiBD/src/main/java/com/joaovictormundel/api/controller/CandidatoController.java
@@ -1,6 +1,5 @@
 package com.joaovictormundel.api.controller;
 
-
 import com.joaovictormundel.api.model.CandidatoEntity;
 import com.joaovictormundel.api.model.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,25 +7,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("candidatos")
 public class CandidatoController {
 
-    @Autowired
-    private CandidatoRepository repo;
+	@Autowired
+	private CandidatoRepository repo;
 
-    @GetMapping
-    public List<CandidatoEntity> getAll(){
-        return repo.findAll();
-    }
+	@GetMapping
+	public List<CandidatoEntity> getAll() {
+		return repo.findAll();
+	}
 
-    @PostMapping
-    public ResponseEntity<CandidatoEntity> create(@RequestBody CandidatoEntity c){
-        return ResponseEntity.ok(repo.save(c));
-    }
+	@PostMapping
+	public ResponseEntity<CandidatoEntity> create(@RequestBody CandidatoEntity c) {
+		return ResponseEntity.ok(repo.save(c));
+	}
 
-
-
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CandidatoEntity> update(@PathVariable Integer id, @RequestBody CandidatoEntity cNew) {
+		return ResponseEntity.ok(repo.save(cNew));
+	}
 
 }
